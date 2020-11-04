2101dc20a297091a69737d6779bb9419510fa08f
Millad <Milladayoubi@gmail.com>
2020-11-03 22:48:58 -0500

    Output Change

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // Basics on Node.js Asyncronous programming 
-console.log('Starting')
+console.log('*Kenobi Jumps Downs*')
+console.log('General Kenobi: "Hello There"')
 
     setTimeout(() => {
         console.log('2 Second Timer')
 
     }, 2000)
 
-    console.log('Stopping')
+    console.log('*Grevious Turns Around*')
+
+console.log('Grevious: "General Kenobi"')
