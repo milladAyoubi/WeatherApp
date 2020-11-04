224fd6686f1d3b12884a528fb7bec1e90ca1a456
Millad <Milladayoubi@gmail.com>
2020-11-03 23:22:03 -0500

    JSON Parse

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const request = require('request')
 
+const url = 'http://api.weatherstack.com/current?access_key=fd4ab5def50a0a0c8d0cfcff7f835846&query=37.8267,-122.4233'
+
+
+request({url: url }, (error, response) =>{
+
+   const data = JSON.parse(response.body)
+   console.log(data.location.name)
+
+})
