39ae623c7596bb692a4af8971406f8ae264674f5
Millad <Milladayoubi@gmail.com>
2020-11-04 19:50:37 -0500

    Current Temp

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=fd4ab5def50a0a0c8d0cfcff7f835846&query=37.8267,-122.4233'
 
-
+console.log('Welcome To WeatherApp W/ WeatherStack API')
+console.log('------------------------------------------')
 request({url: url }, (error, response) =>{
 
    const data = JSON.parse(response.body)
-   console.log(data.current)
+   console.log(' > Current Temperature is ' + data.current.temperature + ' C')
 
 })
