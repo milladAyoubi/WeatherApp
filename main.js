952e60e2d830c2c3c684e7eb772434618aea41c4
Millad <Milladayoubi@gmail.com>
2020-11-04 20:06:43 -0500

    WeatherType

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const request = require('request')
-
+//Refer to WeatherStack API Documentaion for Additional Functions:)
 const url = 'http://api.weatherstack.com/current?access_key=fd4ab5def50a0a0c8d0cfcff7f835846&query=37.8267,-122.4233'
 
 console.log('Welcome To WeatherApp W/ WeatherStack API')
@@ -7,8 +7,14 @@ console.log('------------------------------------------')
 request({url: url }, (error, response) =>{
 
    const data = JSON.parse(response.body)
+   var weatherType = data.current.weather_descriptions[0] 
+   
+
+
    console.log('Its ' + data.current.observation_time)
    console.log(' > Current Temperature is ' + data.current.temperature + ' C')
-   console.log('Chance of Rain is ' + data.current.precip + ' %')
+   console.log(' > Chance of Rain is ' + data.current.precip + ' %')
+   if(weatherType == 'Partly cloudy')
+       console.log('It may be ' + weatherType + ' today but dont let that get you down!')
 
 })
