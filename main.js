22a3bac01ff15450c0fa98147749e5ac8b6bdeab
Millad <Milladayoubi@gmail.com>
2020-11-19 23:06:27 -0500

    Callback Final

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const request = require("request");
+/*
 //Refer to WeatherStack API Documentaion for Additional Functions:)
 const url =
   "http://api.weatherstack.com/current?access_key=fd4ab5def50a0a0c8d0cfcff7f835846&query=37.8267,-122.4233";
@@ -41,22 +42,26 @@ request({ url: url }, (error, response) => {
 */
 
 
-const geocode = (address,callback) => {
+const geoCode = (address,callback) => {
 
-  const mapBox =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1Ijoibmlja29saWNlMzk2MyIsImEiOiJja2g0NTUxNjcxNXB0MnJscWUwY3g4YTF4In0.rxPSADky4xlbyZx2V7Xw5g";
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoibmlja29saWNlMzk2MyIsImEiOiJja2g0NTUxNjcxNXB0MnJscWUwY3g4YTF4In0.rxPSADky4xlbyZx2V7Xw5g';
 
-  request({url: mapBox, json:true}, (error,response) => {
-    if(error) 
+  request({url: url, json:true}, (error,response) => {
+    if(error) {
       callback('Unable To connect to location Service', undefined);
-    else if (response.body.features.length === 0 )
+    }
+    else if (response.body.features.length === 0 ) {
       callback('Unable To Find Location',undefined) 
-    else (undefined, {
+    }
+    else {
+
+    callback(undefined, {
         latitude: response.body.features[0].center[0],
         longitude: response.body.features[0].center[1],
         location: response.body.features[0].place_name
 
     })
+  }
 
 
   });
@@ -64,12 +69,15 @@ const geocode = (address,callback) => {
 
 
 
-geocode('New York',(error,data) => {
-  console.log('Error',error)
+geoCode('Philidelphia',(error,data) => {
+
   console.log('Data',data)
 
 
+
 });
+
+
 
 
 
