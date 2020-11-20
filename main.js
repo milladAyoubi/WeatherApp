6ba4406ce2acf2bd9a4f2225e3ba2ff15cee8b7d
Millad <Milladayoubi@gmail.com>
2020-11-19 22:53:43 -0500

    Callback Exapnsion

--- main.js
+++ main.js
@@ -25,9 +25,7 @@ request({ url: url }, (error, response) => {
       "It may be " + weatherType + " today but dont let that get you down!"
     );
   });
-  console.log("");
-  console.log("Los Angeles Weather Information");
-  console.log("--------------------------------");
+
 /*
   request({ url: mapBox, json: true }, (error, response) => {
     if(error) {console.log('Unable to Connect to Service');}
@@ -48,11 +46,28 @@ const geocode = (address,callback) => {
   const mapBox =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1Ijoibmlja29saWNlMzk2MyIsImEiOiJja2g0NTUxNjcxNXB0MnJscWUwY3g4YTF4In0.rxPSADky4xlbyZx2V7Xw5g";
 
+  request({url: mapBox, json:true}, (error,response) => {
+    if(error) 
+      callback('Unable To connect to location Service', undefined);
+    else if (response.body.features.length === 0 )
+      callback('Unable To Find Location',undefined) 
+    else (undefined, {
+        latitude: response.body.features[0].center[0],
+        longitude: response.body.features[0].center[1],
+        location: response.body.features[0].place_name
 
+    })
+
+
+  });
 };
 
 
+
 geocode('New York',(error,data) => {
+  console.log('Error',error)
+  console.log('Data',data)
+
 
 });
 
