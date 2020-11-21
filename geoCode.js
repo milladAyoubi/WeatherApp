ad1e5c49651aa8c9ab6fee0b8d00c7d45fe65b5b
Millad <Milladayoubi@gmail.com>
2020-11-21 02:42:15 -0500

    Destructuring / Refactoring

--- geoCode.js
+++ geoCode.js
@@ -7,19 +7,19 @@ const geoCode = (address,callback) => {
 
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoibmlja29saWNlMzk2MyIsImEiOiJja2g0NTUxNjcxNXB0MnJscWUwY3g4YTF4In0.rxPSADky4xlbyZx2V7Xw5g';
   
-    request({url: url, json:true}, (error,response) => {
+    request({url, json:true}, (error,{body}) => {
       if(error) {
-        callback('Unable To connect to location Service', undefined);
+        callback('Error', undefined);
       }
-      else if (response.body.features.length === 0 ) {
-        callback('Unable To Find Location',undefined) 
+      else if (body.features.length === 0 ) {
+        callback('Error',undefined) 
       }
       else {
   
       callback(undefined, {
-          latitude: response.body.features[0].center[0],
-          longitude: response.body.features[0].center[1],
-          location: response.body.features[0].place_name
+          latitude: body.features[0].center[0],
+          longitude: body.features[0].center[1],
+          location: body.features[0].place_name
   
       })
     }
