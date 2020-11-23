28fa4382fde4a7b9740536599c517fc2f446b2f3
Millad <Milladayoubi@gmail.com>
2020-11-23 00:05:48 -0500

    HTTP Update

--- HTTP.js
+++ HTTP.js
@@ -4,6 +4,35 @@ const http = require('http')
 
 const url = 'http://api.weatherstack.com/current?access_key=fd4ab5def50a0a0c8d0cfcff7f835846&query=40,-75&units=f' 
 
-http.request(url, (response) =>) {
-    
-}
+const request = http.request(url, (response) => {
+
+
+   
+
+   
+
+    var data = ''
+
+    response.on('data', (chunk) => {
+
+        data = data + chunk.toString()
+
+ 
+
+
+
+
+
+    })
+
+    response.on('end', () => {
+      var body = JSON.parse(data)
+      console.log(data)
+
+    })  
+})
+request.end()
+
+
+
+
